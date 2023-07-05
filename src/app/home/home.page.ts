22d16b88ddd0a624644cab9dd301ebd2915db6b8
jjvasquez92 <jjvasquez@unicauca.edu.co>
2023-07-04 23:03:28 -0500

    cargando datos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,5 +9,20 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
+  
+  imageSource:any;
+
   constructor() {}
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source:CameraSource.Prompt
+    });
+
+    this.imageSource=image.dataUrl;
+  
+    
+  };
 }
